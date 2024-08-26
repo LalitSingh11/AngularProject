3f149900f20b2d0f91978457b02cebf828f9a289
Lalit Singh <lsingh@ex2india.com>
2024-08-26 11:42:01 +0530

    first commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../Shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomato', 6),
+  ];
+
+  constructor() {}
+}
